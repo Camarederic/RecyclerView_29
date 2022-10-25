6baaab96f5bfb73fa76f9d4ca31a2741ee85f851
Camarederic <camarederic@mail.ru>
2022-10-25 23:16:44 +0300

    Create PlantAdapter and plant item

---
+++ app/src/main/java/com/example/recyclerview_29/PlantAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.recyclerview_29
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview_29.databinding.PlantItemBinding
+
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantViewHolder>() {
+
+    val plantList = ArrayList<Plant>()
+
+    class PlantViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        private val binding = PlantItemBinding.bind(item)
+        fun bind(plant: Plant) {
+            binding.imageViewPlant.setImageResource(plant.imageId)
+            binding.textViewTitle.text = plant.title
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.plant_item, parent, false)
+        return PlantViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PlantViewHolder, position: Int) {
+        holder.bind(plantList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return plantList.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun addPlant(plant: Plant){
+        plantList.add(plant)
+        notifyDataSetChanged()
+    }
+}
